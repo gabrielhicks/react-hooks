e1cf933231fdc67bbdd798ed19404126406ef17b
gabrielhicks <gabrielhicks@mail.com>
2021-02-07 21:50:30 -0500

    exec1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
